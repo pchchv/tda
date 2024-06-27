a5806ec700dfce3ca4e155c6bea0101c92920628
pchchv <ipchchv@gmail.com>
2024-06-27 21:02:59 +0300

    Add empty handlers

--- server.go
+++ server.go
@@ -10,10 +10,30 @@ func pingHandler(c fiber.Ctx) error {
 	return c.SendString("To do app service. Version 0.0.0")
 }
 
+func indexHandler(c fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func postHandler(c fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func putHandler(c fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
+func deleteHandler(c fiber.Ctx) error {
+	return c.SendString("Hello")
+}
+
 func server() {
 	app := fiber.New()
 
 	app.Get("/ping", pingHandler)
+	app.Get("/", indexHandler)
+	app.Post("/", postHandler)
+	app.Put("/update", putHandler)
+	app.Delete("/delete", deleteHandler)
 
 	log.Fatal(app.Listen(":" + getEnvValue("PORT")))
 }
