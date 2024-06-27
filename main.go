be68b2f3752ef0bc2835402183566fd0131ec672
pchchv <ipchchv@gmail.com>
2024-06-27 20:51:34 +0300

    Implement environment variable retrieval

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// Load values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Panic("No .env file found")
+	}
+}
+
+func getEnvValue(v string) string {
+	// Getting a value. Outputs a panic if the value is missing
+	value, exist := os.LookupEnv(v)
+	if !exist {
+		log.Panic("Value " + v + "does not exist")
+	}
+	return value
+}
+
+func main() {
+}
